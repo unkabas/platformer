54ef1064ad5c4bec807804f252ff2bd15da9f1e6
unkabas <undercannabis10@gmail.com>
2024-12-29 14:34:14 +0300

    Create SpeedTrigger.cs

---
+++ SpeedTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedTrigger : MonoBehaviour
+{
+
+    public float speedFactor = 2.5f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Увеличение скорости
+        other.GetComponent<FirstPersonMovement>().runSpeed *= speedFactor;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<FirstPersonMovement>().runSpeed /= speedFactor;
+
+    }
+}
