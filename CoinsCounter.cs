faf3480a7b6495b02be617732088ed371ab9bfe5
unkabas <undercannabis10@gmail.com>
2025-01-26 03:19:30 -0800

    Create CoinsCounter.cs

---
+++ CoinsCounter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class CoinsCounter : MonoBehaviour
+{
+    //Число собранных монет
+    public int coins;
+ 
+    //Метод, увеличивающий число монеток
+    public void CollectCoins()
+    {
+        coins++;
+    }
+}
